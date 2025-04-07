df724aed2ead1a4067e0dff06b76d2728d9b63ff
Chocs <Luisitom@gmail.com>
2025-04-06 23:19:29 -0600

    [Add] Se agregan las ultimas funciones de administrador

--- nuevo_destino.js
+++ nuevo_destino.js
@@ -1,36 +1,51 @@
 document.getElementById('btnRegresar').addEventListener('click', function () {
     window.location.href = 'admin.html';
-  });
+});
 
-  document.getElementById('formDestino').addEventListener('submit', function (e) {
+document.getElementById('formDestino').addEventListener('submit', function (e) {
     e.preventDefault();
-    const json = JSON.parse(localStorage.getItem('wondersData')) || [];
+
+    // Obtener datos desde localStorage y sessionStorage
+    let json = [];
+
+    // Obtener el JSON desde sessionStorage
+    json = JSON.parse(sessionStorage.getItem('lugares')) || [];
+    console.log("sessionStorage", json); 
+
 
     const name = document.getElementById('name').value;
     const description = document.getElementById('description').value;
     const link_sitio = document.getElementById('link_sitio').value;
     const link_ubicacion = document.getElementById('link_ubicacion').value;
-    
-    // Procesar las rutas de imágenes
+
+    // Procesar rutas de imágenes
     const imagenesInput = document.getElementById('imagenes').value;
     const imagenes = imagenesInput.split(',').map(ruta => ({
-      image: ruta.trim()
+        image: ruta.trim()
     }));
 
     const filtro = document.getElementById('filtro').value.trim();
 
     const destino = {
-      name,
-      description,
-      imagenes,
-      link_sitio,
-      link_ubicacion,
-      filtro
+        name,
+        description,
+        imagenes,
+        link_sitio,
+        link_ubicacion,
+        filtro
     };
-    
+
+
+
+    // Agregar al array de sessionStorage
     json.push(destino);
-    localStorage.setItem('wondersData', JSON.stringify(json));
+    sessionStorage.setItem('lugares', JSON.stringify(json));
+
+    // Mostrar el nuevo destino en pantalla
     document.getElementById('outputJson').textContent = JSON.stringify(destino, null, 2);
-    console.log('json', json);
+
+
+    console.log('sessionStorage json', sessionStorage.getItem('lugares'));
+
     e.target.reset();
-  });
+});
